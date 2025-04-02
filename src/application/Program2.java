083f8cc1af1b1ff213d84e7384481c07da0504e7
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-04-02 14:20:53 -0300

    department Update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -13,18 +13,28 @@ public class Program2 {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-		System.out.println("===== TEST 2: department finById ======");
+		System.out.println("===== TEST 1: department finById ======");
 
 		Department department = departmentDao.findById(1);
 		System.out.println(department);
 
 		System.out.println();
 		
-		System.out.println("===== TEST 1: department delete by id ======");
+		System.out.println("===== TEST 2: department delete by id ======");
 		System.out.print("Enter id value for department delete");
 		int id = sc.nextInt();
 		departmentDao.deleteById(id);
 		System.out.println("Delete was completed");
+		
+		System.out.println();
+		
+		System.out.println("===== TEST 3: department update ======");
+		department = departmentDao.findById(3);
+		department.setName("Music");
+		departmentDao.update(department);
+		System.out.println(department);
+		
+	
 
 		sc.close();
 	}
