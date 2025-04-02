cd57296cca1281938cab18b61772a75e46399d12
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-04-02 15:26:10 -0300

    department FindAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -19,28 +21,34 @@ public class Program2 {
 		System.out.println(department);
 
 		System.out.println();
-		
-		/*System.out.println("===== TEST 2: department delete by id ======");
+
+		System.out.println("===== TEST 2: department delete by id ======");
 		System.out.print("Enter id value for department delete");
 		int id = sc.nextInt();
 		departmentDao.deleteById(id);
-		System.out.println("Delete was completed");*/
-		
+		System.out.println("Delete was completed");
+
 		System.out.println();
-		
+
 		System.out.println("===== TEST 3: department update ======");
 		department = departmentDao.findById(3);
 		department.setName("Music");
 		departmentDao.update(department);
 		System.out.println(department);
 		
+		System.out.println();
+
 		System.out.println("===== TEST 4: department insert ======");
 		Department departamento1 = new Department(null, "Fashion");
 		departmentDao.insert(departamento1);
 		System.out.println("New department id: " + departamento1.getId());
 
-		
-	
+		System.out.println();
+
+		System.out.println("===== TEST 5: department findAll ======");
+		List<Department> depp = new ArrayList<>();
+		depp = departmentDao.findAll();
+		System.out.println(depp + "\n");
 
 		sc.close();
 	}
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -136,8 +137,30 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			List<Department> dep = new ArrayList<>();
+			st = conn.prepareStatement("SELECT * FROM department");
+
+			
+
+			rs = st.executeQuery();
+
+			while(rs.next()) {
+				Department obj = instantiateDepartment(rs);
+				dep.add(obj);
+			}
+			
+			return dep;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
